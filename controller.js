cabda0ee1767957c5f6eeff2bfebca9fb484afd6
Christopher <thorstedc266935@gmail.com>
2017-07-18 12:57:06 -0500

    usersmini

--- controller.js
+++ controller.js
@@ -1,5 +1,10 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular.module('userprofiles').controller('MainController', function($scope, mainSrvc){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+   $scope.getUsers = function() {
+     $scope.users = mainSrvc.getUsers();
+   }
+
+   $scope.getUsers();
+   $scope.toggleFavorite = mainSrvc.toggleFavorite;
 })
